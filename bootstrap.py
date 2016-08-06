775720cfb90fe09ea83c789a8b19c2143b4adf4b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-08-06 18:55:34 +0100

    Added limited compiler selection

--- bootstrap.py
+++ bootstrap.py
@@ -1,12 +1,31 @@
+import sys
+import os.path
 import zipfile
 import urllib.request
 
-print("CodeSmithy bootstrap build")
+print("\nCodeSmithy bootstrap build")
+print("--------------------------\n")
 
-print("Fetching CodeSmithy code from https://github.com/CodeSmithyIDE/CodeSmithy/archive/master.zip")
+print("Step 1a: Fetching CodeSmithy code from https://github.com/CodeSmithyIDE/CodeSmithy/archive/master.zip")
 urllib.request.urlretrieve('https://github.com/CodeSmithyIDE/CodeSmithy/archive/master.zip', 'master.zip')
 
-print("Unzipping master.zip")
+print("Step 1b: Unzipping master.zip\n")
 zip_ref = zipfile.ZipFile('master.zip', 'r')
 zip_ref.extractall('.')
 zip_ref.close()
+
+print("Step 2: Finding compilers")
+compilers = []
+foundMSVC14 = os.path.isfile('C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe')
+if foundMSVC14:
+    compilers.append("Visual Studio 2015")
+
+print("The following compilers have been found")
+for i, c in enumerate(compilers):
+    print(str(i+1) + ") " + c)
+
+if len(compilers) == 0:
+    print("No suitable compilers found, exiting")
+    sys.exit();
+
+selectedCompiler = (int(input("Select the compiler to use: ")) - 1)
