61f210021a6707753f2e6110a47c4143c5b3dfff
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-08-07 17:49:19 +0100

    Added CodeSmithyCore build

--- bootstrap.py
+++ bootstrap.py
@@ -20,18 +20,18 @@ else:
     print("Architecture: 32 bit")
 print("")
 
-print("Step 1a: Fetching libgit2 code from https://github.com/CodeSmithyIDE/libgit2/archive/master.zip")
+print("Step 1a: Fetching libgit2 code from https://github.com/CodeSmithyIDE/libgit2/archive/master.zip", flush=True)
 urllib.request.urlretrieve("https://github.com/CodeSmithyIDE/libgit2/archive/master.zip", "libgit2-master.zip")
 
-print("Step 1b: Unzipping libgit2-master.zip\n")
+print("Step 1b: Unzipping libgit2-master.zip\n", flush=True)
 zip_ref = zipfile.ZipFile("libgit2-master.zip", "r")
 zip_ref.extractall(".")
 zip_ref.close()
 
-print("Step 1c: Fetching CodeSmithy code from https://github.com/CodeSmithyIDE/CodeSmithy/archive/master.zip")
+print("Step 1c: Fetching CodeSmithy code from https://github.com/CodeSmithyIDE/CodeSmithy/archive/master.zip", flush=True)
 urllib.request.urlretrieve("https://github.com/CodeSmithyIDE/CodeSmithy/archive/master.zip", "CodeSmithy-master.zip")
 
-print("Step 1d: Unzipping CodeSmithy-master.zip\n")
+print("Step 1d: Unzipping CodeSmithy-master.zip\n", flush=True)
 zip_ref = zipfile.ZipFile("CodeSmithy-master.zip", "r")
 zip_ref.extractall(".")
 zip_ref.close()
@@ -50,13 +50,13 @@ for i, c in enumerate(compilers):
 
 if len(compilers) == 0:
     print("No suitable compilers found, exiting")
-    sys.exit();
+    sys.exit(-1);
 
 selectedCompiler = (int(input("Select the compiler to use: ")) - 1)
 print("")
 
 # CMake is not easily buildable on Windows so we rely on a binary distribution
-print("Step 3: Installing CMake\n")
+print("Step 3: Installing CMake\n", flush=True)
 cmakePath = ""
 if platformName == "Windows":
     if is64bit:
@@ -68,15 +68,38 @@ if platformName == "Windows":
     zip_ref.extractall(".")
     zip_ref.close()
 
-print("Step 4: Building libgit2")
+print("Step 4: Building libgit2", flush=True)
 rc = subprocess.call([cmakePath, "--build", "libgit2-master"])
 if rc == 0:
     print("libgit2 build successfully")
+else:
+    sys.exit(-1)
 
-print("Step 5: Building CodeSmithyMake")
+print("Step 5: Building CodeSmithyCore", flush=True)
+codeSmithyCoreMakefilePath = ""
+if compilers[selectedCompiler] == "Visual Studio 2015":
+    codeSmithyCoreMakefilePath = "CodeSmithy-master/Core/Makefiles/VC14/CodeSmithyCore.sln"
+rc = subprocess.call([compilerPaths[selectedCompiler], codeSmithyCoreMakefilePath, "/build", "Debug"])
+if rc == 0:
+    print("CodeSmithyCore built successfully")
+else:
+    print("Failed to build CodeSmithyCore, exiting")
+    sys.exit(-1)
+    
+print("Step 6: Building CodeSmithyMake", flush=True)
 codeSmithyMakeMakefilePath = ""
 if compilers[selectedCompiler] == "Visual Studio 2015":
     codeSmithyMakeMakefilePath = "CodeSmithy-master/Make/Makefiles/VC14/CodeSmithyMake.sln"
 rc = subprocess.call([compilerPaths[selectedCompiler], codeSmithyMakeMakefilePath, "/build", "Debug"])
 if rc == 0:
     print("CodeSmithyMake built successfully")
+else:
+    sys.exit(-1)
+    
+print("Step 7: Build CodeSmithy", flush=True)
+rc = subprocess.call(["CodeSmithy-master/Make/Makefiles/VC14/Debug/CodeSmithyMake.exe"])
+if rc == 0:
+    print("CodeSmithy built successfully")
+else:
+    sys.exit(-1)
+    
