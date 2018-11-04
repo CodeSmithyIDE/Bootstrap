8431750f3c8d14a8f03632c3e9cec00578b6d22d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2018-11-04 22:20:58 +0000

    Updates

--- state.py
+++ state.py
@@ -2,13 +2,20 @@ import json
 
 class State:
     def __init__(self):
+        self.previousStateFound = False
         self.downloadComplete = False
         try:
-            with open('state.json', 'r+') as file:
+            with open('state.json', 'r') as file:
                 state = json.loads(file.read())
-                self.downloadComplete = state.downloadComplete
+                self.downloadComplete = state["downloadComplete"]
+                self.previousStateFound = True
         except:
             self.save()
+
+    def reset(self):
+        self.previousStateFound = False
+        self.downloadComplete = False
+        self.save()
 
     def setDownloadComplete(self):
         self.downloadComplete = True
