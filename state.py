ab5da47ab92c84de418f5363e9d32f8afbdabb2b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2018-11-04 21:58:29 +0000

    Updates

--- state.py
+++ state.py
@@ -1,3 +1,20 @@
+import json
+
 class State:
     def __init__(self):
-        pass
+        self.downloadComplete = False
+        try:
+            with open('state.json', 'r+') as file:
+                state = json.loads(file.read())
+                self.downloadComplete = state.downloadComplete
+        except:
+            self.save()
+
+    def setDownloadComplete(self):
+        self.downloadComplete = True
+        self.save()
+
+    def save(self):
+        with open('state.json', 'w+') as file:
+            state = { "downloadComplete": self.downloadComplete }
+            file.write(json.dumps(state))
