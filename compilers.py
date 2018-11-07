888e05a22cc19cd92c1933ff1a8493fc262393a8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2018-11-07 00:52:43 +0000

    Updates

--- compilers.py
+++ compilers.py
@@ -1,22 +1,29 @@
 import os.path
+import subprocess
+
+class Compiler:
+    def __init__(self, name, short_name, executable):
+        self.name = name
+        self.short_name = short_name
+        self.executable = executable
+
+    def compile(self, makefilePath):
+        return subprocess.call([self.executable, makefilePath, "/build", "Debug"])
 
 class Compilers:
     def __init__(self):
         self.compilers = []
-        self.compilerPaths = []
         foundMSVC14 = os.path.isfile("C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe")
         if foundMSVC14:
-            self.compilers.append("Visual Studio 2015")
-            self.compilerPaths.append("C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe")
+            self.compilers.append(Compiler("Visual Studio 2015", "VC14", "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/IDE/devenv.exe"))
         foundMSVC2017 = os.path.isfile("C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe")
         if foundMSVC2017:
-            self.compilers.append("Visual Studio 2017")
-            self.compilerPaths.append("C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe")
+            self.compilers.append(Compiler("Visual Studio 2017", "VC15", "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/devenv.exe"))
 
     def showCompilerList(self):
         if len(self.compilers) != 0:
             print("    The following compilers have been found")
             for i, compiler in enumerate(self.compilers):
-                print("        " + str(i+1) + ") " + compiler)
+                print("        " + str(i+1) + ") " + compiler.name)
         else:
             print("    No compilers have been found")
