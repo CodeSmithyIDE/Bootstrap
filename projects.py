14f16246c31e5989514ef3dc53b20212f2cb48bd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2018-11-07 21:46:20 +0000

    Updates

--- projects.py
+++ projects.py
@@ -1,3 +1,23 @@
+class Project:
+    def __init__(self, name):
+        self.name = name
+
+    def build(self, compiler):
+        print("Step 6: Building Process", flush=True)
+        processMakefilePath = "Build/Ishiko/Process/Makefiles/" + compiler.short_name + "/IshikoProcess.sln"
+        rc = compiler.compile(processMakefilePath)
+        if rc == 0:
+            print("Process built successfully")
+        else:
+            print("Failed to build Process, exiting")
+            sys.exit(-1)
+        print("")
+
 class Projects:
     def __init__(self):
-        pass
+        self.projects = []
+        self.projects.append(Project("Ishiko/Process"))
+
+    def build(self, compiler):
+        for project in self.projects:
+            project.build(compiler)
