dfdb82574d6da3ae195ab80f6d652a6e8d6bd580
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2018-11-14 23:14:54 +0000

    Updates

--- codesmithymake.py
+++ codesmithymake.py
@@ -1,6 +1,12 @@
+import os
+import subprocess
+
 class CodeSmithyMake:
     def __init__(self):
-        pass
+        self.executable = os.getcwd() + "/Build/CodeSmithyIDE/CodeSmithy/Bin/Win32/CodeSmithyMake.exe"
 
     def build(self, makefile_path):
-        pass
+        try:
+            subprocess.check_call([self.executable, makefile_path])
+        except subprocess.CalledProcessError:
+            raise RuntimeError("Compilation of " + makefile_path + " failed.")
