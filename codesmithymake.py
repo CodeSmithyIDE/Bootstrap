b37a42bd08411ed28ba4adb09210b0d53b8c0004
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2018-11-21 00:42:35 +0000

    Fix

--- codesmithymake.py
+++ codesmithymake.py
@@ -14,7 +14,7 @@ class CodeSmithyMake:
         try:
             subprocess.check_call([self.executable, "-p", configuration, makefile_path])
         except subprocess.CalledProcessError:
-            launchIDE = input.query("    Compilation failed. Do you you want to launch the IDE? [y/n]", ["y", "n"])
+            launchIDE = input.query("    Compilation failed. Do you you want to launch the IDE?", ["y", "n"], "n")
             if launchIDE == "y":
                 compiler.launch(makefile_path)
             raise RuntimeError("Compilation of " + makefile_path + " failed.")
