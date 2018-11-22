28409eb23ddc676a4bf800df56c48df0b3f00d4c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2018-11-22 22:51:47 +0000

    Updates

--- input.py
+++ input.py
@@ -2,10 +2,12 @@ class Input:
     def query(self, question, valid_answers, default):
         question += self._formatted_answers(valid_answers, default)
         user_answer = None
-        while user_answer not in valid_answers:
-            user_answer = input(question)
-            if user_answer == "":
+        while user_answer is None:
+            user_input = input(question)
+            if user_input == "":
                 user_answer = default
+            else:
+                user_answer = self._lowercase_find(valid_answers, user_input)
         return user_answer
 
     def _formatted_answers(self, valid_answers, default):
@@ -20,3 +22,12 @@ class Input:
             formatted_answers.append(formatted_answer)
         separator = "/"
         return " [" + separator.join(formatted_answers) + "] "
+
+    def _lowercase_find(self, valid_answers, answer):
+        user_answer = None
+        for valid_answer in valid_answers:
+            if answer.lower() == valid_answer.lower():
+                user_answer = valid_answer
+                break
+        return user_answer
+                
