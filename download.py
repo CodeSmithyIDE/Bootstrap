dc894256c3d5d6603495adfa77b769aa44d8cb13
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2018-12-23 16:53:46 +0000

    Refactoring

--- download.py
+++ download.py
@@ -33,18 +33,27 @@ class Download:
         Path(self.download_path).parent.mkdir(parents=True, exist_ok=True)
         urllib.request.urlretrieve(self.url, self.download_path)
 
-    def unzip(self):
+    def unzip(self, destination_dirs=None):
+        """Creates a downloader to download the package(s) for this project.
+
+        Parameters
+        ----------
+        destination_dirs
+            A list of directories where the package should be unzipped.
+        """
+
         if not self.unzipped:
             print("    Unzipping " + self.download_path, flush=True)
-            shutil.rmtree(self.extract_path_prefix + "/" + self.name,
-                          ignore_errors=True)
-            shutil.rmtree(self.extract_path_prefix + "/" + self.name + "-" +
-                          self.branch, ignore_errors=True)
+            if destination_dirs is None:
+                destination_dirs = [self.extract_path_prefix + "/" + self.name]
+            destination_dir = destination_dirs[0]
+            temp_destination_dir = self.extract_path_prefix + "/" + self.name + "-" + self.branch
+            shutil.rmtree(destination_dir, ignore_errors=True)
+            shutil.rmtree(temp_destination_dir, ignore_errors=True)
             zip_ref = zipfile.ZipFile(self.download_path, "r")
             zip_ref.extractall(self.extract_path_prefix)
             zip_ref.close()
-            os.rename(self.extract_path_prefix + "/" + self.name + "-" +
-                      self.branch, self.extract_path_prefix + "/" + self.name)
+            os.rename(temp_destination_dir, destination_dir)
         else:
             print("    " + self.download_path + " already unzipped",
                   flush=True)
